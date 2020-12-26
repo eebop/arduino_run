e50b5e5ba2c93e2d28741055c1a037f5a375bb4c
eebop <68796004+eebop@users.noreply.github.com>
2020-12-26 14:20:42 -0800

    Update arduino_run.sh

--- arduino_run.sh
+++ arduino_run.sh
@@ -1,6 +1,6 @@
-WORKDIR=/tmp/kieran
+WORKDIR=/tmp/arduino
 mkdir -p $WORKDIR
-cp $1 $WORKDIR/kieran.ino
+cp $1 $WORKDIR/arduino.ino
 arduino-cli compile --fqbn arduino:avr:uno $WORKDIR > /dev/null
 arduino-cli upload -p /dev/cu.usbmodem14201 --fqbn arduino:avr:uno $WORKDIR
 rm -rf $WORKDIR
